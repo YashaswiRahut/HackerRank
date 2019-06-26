06225902cb44e6c8428a48b115be2d6eacc4c419
Yashaswi Rahut <yashaswirahut02@gmail.com>
2019-06-26 10:01:00 +0530

    Update StringsMakingAnagrams.cpp

--- StringsMakingAnagrams.cpp
+++ StringsMakingAnagrams.cpp
@@ -4,31 +4,14 @@ using namespace std;
 
 // Complete the makeAnagram function below.
 int makeAnagram(string a, string b) {
-    map<char,int> a1;
-    map<char,int> b1;
-    int i,n=0;
-    for(i=0;i<a.size();i++)
-    {
-        a1.insert(pair<char, int>(a[i],));
-        a1[i].second++;
-    }
-    for(i=0;i<b.size();i++)
-    {
-        b1[i].first= a[i];
-        b1[i].second++;
-    }
-    if(a1.size()>b1.size())
-    {
-        n=a1.size()-b1.size();
-        for(i=0;i<b1.size();i++)
-        {
-            if(a[i].second-b[a[i].first]>0)
-                n+=a[i].second-b[a[i].first];
-            else
-                n+=b[a[i].first]-a[i].second;
-        }
-    }
-
+    auto n=0;
+    vector<int> count(26,0);
+    for(auto i:a)
+        ++count[i-'a'];
+    for(auto i:b)
+        --count[i-'a'];
+    for(auto i:count)
+        n+=count[i];
     return n;
 }
 
